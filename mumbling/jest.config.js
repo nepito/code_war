95f28a57cced81fdede94af53e76e8a728c57166
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-08-11 20:33:51 -0700

    Quita los snapshot

--- mumbling/jest.config.js
+++ mumbling/jest.config.js
@@ -4,10 +4,6 @@ module.exports = {
     {
       displayName: "test",
       preset: "jest-preset-gatsby/typescript",
-      snapshotSerializers: [
-        "jest-serializer-react-helmet",
-        "jest-serializer-json-ld-script",
-      ],
       collectCoverage: true,
     },
   ],
