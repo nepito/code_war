edec0d35a5c214007453cd7c750e22291582387a
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-03 12:38:08 -0700

    Prueba que la salida es un arreglo

--- cryptanalysis_word_patterns/tests/tests_nepito.test.js
+++ cryptanalysis_word_patterns/tests/tests_nepito.test.js
@@ -3,4 +3,11 @@ describe("Function string2array convert an string to an array", function() {
     test("should be a function", function(){
         expect(typeof cwp.string2array).toBe("function");
     });
+    test("should return an array", function(){
+        let expected = "Array";
+        let outout = cwp.string2array("hello");
+        let obtained = typeof(outout);
+        expect(expected).toBe(obtained);
+
+    });
 });
