e565c5a032060c2a1fc5347285fd444a7723e42c
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-03 16:56:49 -0700

    Borra código obsoleto
    Pensaba que tenía que contar, pero solo debo de decir cuál es el elemento

--- cryptanalysis_word_patterns/src/index.js
+++ cryptanalysis_word_patterns/src/index.js
@@ -9,32 +9,8 @@ function string2setarray( string ){
     return salida
 }
 
-function count_each_element( arreglo ){
-    var i_elemento;
-    var cuenta = [];
-    var elementos = [...new Set(arreglo)]
-    for (i_elemento = 0; i_elemento < elementos.length; i_elemento++) {
-        var cuenta_elemento = count_element(arreglo, elementos[i_elemento]);
-        cuenta.push(cuenta_elemento);
-    }
-    return cuenta
-};
-
-function count_element(arreglo, elemento){
-    var i_elemento;
-    var cuenta = 0;
-    for (i_elemento = 0; i_elemento < arreglo.length; i_elemento++) {
-        if (arreglo[i_elemento] == elemento) {
-            cuenta++;
-        }
-    }
-    return cuenta
-};
-
 
 module.exports = {
   wordPattern: wordPattern,
   string2setarray: string2setarray,
-  count_each_element:count_each_element,
-  count_element:count_element,
 };
--- cryptanalysis_word_patterns/tests/tests_nepito.test.js
+++ cryptanalysis_word_patterns/tests/tests_nepito.test.js
@@ -16,37 +16,3 @@ describe("Function string2setarray convert an string to an set array", function(
         expect(expected).toStrictEqual(obtained);
     });
 });
-
-describe("Should count how many times is each element", function(){
-    test("Should be a function count_each_element", function(){
-        expect(typeof cwp.count_each_element).toBe("function");
-    });
-    test("Shoul count how many times is each element", function(){
-        var expected = [1];
-        var obtained = cwp.count_each_element(["A"]);
-        expect(expected).toStrictEqual(obtained);
-        var expected = [1,1];
-        var obtained = cwp.count_each_element(["A", "B"]);
-        expect(expected).toStrictEqual(obtained);
-        var expected = [2];
-        var obtained = cwp.count_each_element(["A", "A"]);
-        expect(expected).toStrictEqual(obtained);
-        var expected = [2,1];
-        var obtained = cwp.count_each_element(["A", "N", "A"]);
-        expect(expected).toStrictEqual(obtained);
-    });
-});
-
-describe("Should count how many times is an element", function(){
-    test("Should be a function count_element", function(){
-        expect(typeof cwp.count_element).toBe("function");
-    });
-    test("Shoul count how many times is each element", function(){
-        var expected = 1;
-        var obtained = cwp.count_element(["A"],"A");
-        expect(expected).toStrictEqual(obtained);
-        var expected = 2;
-        var obtained = cwp.count_element(["A", "A"],"A");
-        expect(expected).toStrictEqual(obtained);
-    });
-});
