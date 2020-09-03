7b41052ccd4df11067c67f0347fbea3c172ca94c
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-03 12:45:30 -0700

    Prueba que la salida es la esperada
    Cambió la prueba porque el tipo era un objeto y no un arreglo

--- cryptanalysis_word_patterns/tests/tests_nepito.test.js
+++ cryptanalysis_word_patterns/tests/tests_nepito.test.js
@@ -3,11 +3,10 @@ describe("Function string2array convert an string to an array", function() {
     test("should be a function", function(){
         expect(typeof cwp.string2array).toBe("function");
     });
-    test("should return an array", function(){
-        let expected = "Array";
-        let outout = cwp.string2array("hello");
-        let obtained = typeof(outout);
-        expect(expected).toBe(obtained);
+    test("should return an right array", function(){
+        let expected = ["A"];
+        let obtained = cwp.string2array("A");
+        expect(expected).toStrictEqual(obtained);
 
     });
 });
