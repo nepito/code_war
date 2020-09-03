dacd9686c6b53d492c3b37f757cda88821f8a151
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-03 12:29:19 -0700

    Agrega la primera prueba para las funciones secundarias

---
+++ cryptanalysis_word_patterns/tests/tests_nepito.test.js
@@ -0,0 +1,5 @@
+describe("Function string2array convert an string to an array", function() {
+    test("should be a function", function(){
+        expect(typeof string2array).toBe("function");
+    });
+});
