25d8c4da807f8c0a9fc17d8b44d1d60dc03bd907
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-03 12:50:01 -0700

    Agrega la segunda prueba

--- cryptanalysis_word_patterns/tests/tests_nepito.test.js
+++ cryptanalysis_word_patterns/tests/tests_nepito.test.js
@@ -4,8 +4,11 @@ describe("Function string2array convert an string to an array", function() {
         expect(typeof cwp.string2array).toBe("function");
     });
     test("should return an right array", function(){
-        let expected = ["A"];
-        let obtained = cwp.string2array("A");
+        var expected = ["A"];
+        var obtained = cwp.string2array("A");
+        expect(expected).toStrictEqual(obtained);
+        expected = ["B"];
+        obtained = cwp.string2array("B");
         expect(expected).toStrictEqual(obtained);
 
     });
