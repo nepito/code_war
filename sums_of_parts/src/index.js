878a4088a03179912852c0de079d137485affc8a
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-04 11:07:10 -0700

    Implementa la función vacía

---
+++ sums_of_parts/src/index.js
@@ -0,0 +1,6 @@
+function partsSums(){};
+
+
+module.exports = {
+    partsSums: partsSums,
+};
--- sums_of_parts/tests/test_nepito.test.js
+++ sums_of_parts/tests/test_nepito.test.js
@@ -1,6 +1,8 @@
+const sop = require("../src/index")
+
 describe("Should be a function", function(){
     test("Should be a function", function(){
-        expect(typeof partsSums).toBe("function")
+        expect(typeof sop.partsSums).toBe("function")
     })
 });
 describe("Should the function sum_elements_array return the sum", function(){
