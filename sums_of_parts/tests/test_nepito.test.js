589691b419b71c441f01f43632cab2afdecad88c
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-09-04 11:03:59 -0700

    Prueba que existe la función partsSums

--- sums_of_parts/tests/test_nepito.test.js
+++ sums_of_parts/tests/test_nepito.test.js
@@ -1,8 +1,12 @@
+describe("Should be a function", function(){
+    test("Should be a function", function(){
+        expect(typeof partsSums).toBe("function")
+    })
+});
 describe("Should the function sum_elements_array return the sum", function(){
     test("", function(){
         var expected = 1;
         var obtained = sum_elements_array([1]);
         expect(obtained).toStrictEqual(expected);
     });
-
 });
