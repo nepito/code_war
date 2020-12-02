cd7885c22be6938dd1316583c779c8230fdcc2c0
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 19:57:54 -0800

    ✅ 🔧 Agrega prueba vacía

--- pascal/src/index.js
+++ pascal/src/index.js
@@ -4,6 +4,12 @@ function pascal( values ){
     return salida
 }
   
+function pascal_2( values ){
+    var salida = [[1]];
+    // Return all non-odd values
+    return salida
+}
+  
 module.exports = {
   pascal : pascal,
 };
--- pascal/tests/tests_nepito.test.js
+++ pascal/tests/tests_nepito.test.js
@@ -1,28 +1,9 @@
 const answer = require("../src/index");
 
-describe.skip( "Testing the function is_pair", function(){
-    it( "should return true for 2", function(){
-      expect(answer.is_pair(2)).toStrictEqual(true);
-    });
-    it( "should return false for 1", function(){
-      expect(answer.is_pair(1)).toStrictEqual(false);
-    });
-});
-
-describe.skip( "Testing the function append_pair", function(){
-    it( "should return [2] for (2,[])", function(){
-      expect(answer.append_pair(2, [])).toStrictEqual([2]);
-    });
-    it( "should return [] for (1,[])", function(){
-      expect(answer.append_pair(1, [])).toStrictEqual([]);
-    });
-    it( "should return [1] for (1,[1])", function(){
-      expect(answer.append_pair(1, [1])).toStrictEqual([1]);
-    });
-    it( "should return [1,8] for (8,[1])", function(){
-      expect(answer.append_pair(8, [1])).toStrictEqual([1, 8]);
-    });
-    it( "should return [1,0] for (undefined,[1])", function(){
-      expect(answer.append_pair(undefined, [1])).toStrictEqual([1, 0]);
-    });
+describe("Should do nothing", function(){
+  test("do nothing", function(){
+    expected = true
+    obtained = true
+    expect(expected).toStrictEqual(obtained);
+  })
 });
