b845f29ed6349f797cf7d63ba163212a892941c8
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 22:11:32 -0800

    ✅ 🔧 Agrega prueba para `make_row`

--- pascal/tests/tests_nepito.test.js
+++ pascal/tests/tests_nepito.test.js
@@ -30,3 +30,16 @@ describe("Test internals_row", function () {
     assess_internals_row([1, 3, 3, 1], expected)
   });
 });
+
+describe("Test make_row", function(){
+  test("should make_row([2]) return [1, 2, 1]", function(){
+    expected = [1, 2, 1];
+    obtained = answer.make_row([2]);
+    expect(expected).toStrictEqual(obtained)
+  });
+  test("should make_row([3, 3]) return [1, 3, 3, 1]", function(){
+    expected = [1, 3, 3, 1];
+    obtained = answer.make_row([3, 3]);
+    expect(expected).toStrictEqual(obtained)
+  });
+});
