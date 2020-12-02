5d3193d5c9374ed0014913b0c2fcd9616b9fe8e9
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 20:17:17 -0800

    🎨 Refactoriza función en las tesis

--- pascal/tests/tests_code_wars.test.ts
+++ pascal/tests/tests_code_wars.test.ts
@@ -1,9 +1,19 @@
 const answer = require("../src/index");
 
-describe( "Testing some inputs", function(){
-    test( "should first row Pascal is [[1]]", function(){
-      expected = [[1]];
-      obtained = answer.pascal(1);
-      expect(expected).toStrictEqual(obtained);
-    });
+describe("Testing some inputs", function () {
+  const deep = 1;
+
+  function assert_pascal_from_deep(deep, expected) {
+    obtained = answer.pascal(deep);
+    expect(expected).toStrictEqual(obtained);
+  }
+
+  test("should first row Pascal is [[1]]", function () {
+    expected = [[1]];
+    assert_pascal_from_deep(1, expected)
+  });
+  test.skip("should first row Pascal is [[1]]", function () {
+    expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
+    assert_pascal_from_deep(2, expected)
+  });
 });
