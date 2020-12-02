342160e13191cdd48b9436d2b15d221dae42ad5a
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 20:22:57 -0800

    📝 Mejora la documentación de la prueba

--- pascal/tests/tests_code_wars.test.ts
+++ pascal/tests/tests_code_wars.test.ts
@@ -1,8 +1,6 @@
 const answer = require("../src/index");
 
 describe("Testing some inputs", function () {
-  const deep = 1;
-
   function assert_pascal_from_deep(deep, expected) {
     obtained = answer.pascal(deep);
     expect(expected).toStrictEqual(obtained);
@@ -12,7 +10,7 @@ describe("Testing some inputs", function () {
     expected = [[1]];
     assert_pascal_from_deep(1, expected)
   });
-  test.skip("should first row Pascal is [[1]]", function () {
+  test.skip("should first five rows Pascal are [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]", function () {
     expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
     assert_pascal_from_deep(2, expected)
   });
