b37482baf8cff377de57314d2a44b38d35cf1915
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 20:33:42 -0800

    ✅ 🔧 Agrega prueba para el segundo nivel

--- pascal/tests/tests_nepito.test.js
+++ pascal/tests/tests_nepito.test.js
@@ -1,9 +1,13 @@
 const answer = require("../src/index");
 
 describe("Should do nothing", function(){
-  test("do nothing", function(){
-    expected = true
-    obtained = true
+  function assert_pascal_from_deep(deep, expected) {
+    obtained = answer.pascal(deep);
     expect(expected).toStrictEqual(obtained);
-  })
+  }
+
+  test("should first row Pascal is [[1]]", function () {
+    expected = [[1], [1, 1]];
+    assert_pascal_from_deep(2, expected)
+  });
 });
