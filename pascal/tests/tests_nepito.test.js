1c8f8ab48f849de821310ccfac0d9283f27685ae
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 21:24:33 -0800

    ✅ 🔧 Agrega prueba para la suma interna

--- pascal/tests/tests_nepito.test.js
+++ pascal/tests/tests_nepito.test.js
@@ -5,9 +5,16 @@ describe("Should do nothing", function(){
     obtained = answer.pascal(deep);
     expect(expected).toStrictEqual(obtained);
   }
-
   test("should first row Pascal is [[1]]", function () {
     expected = [[1], [1, 1]];
     assert_pascal_from_deep(2, expected)
+  });
+});
+
+describe("Test internals_row", function(){
+  test("should return the firt result", function(){
+    expected = [2];
+    obtained = answer.internals_row([1, 1])
+    expect(expected).toStrictEqual(obtained);
   });
 });
