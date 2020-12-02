ea06a67ccad049741b0a467e992c03151eb763d8
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 20:39:11 -0800

    ⚡️ Pasa la prueba del segundo nivel

--- pascal/src/index.js
+++ pascal/src/index.js
@@ -1,5 +1,8 @@
-function pascal( values ){
+function pascal( deep ){
     var salida = [[1]];
+    if (deep ==2) {
+      salida.push([1, 1]);
+    }
     // Return all non-odd values
     return salida
 }
